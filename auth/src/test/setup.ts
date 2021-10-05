d870f59d5b5057e1ba57684a428618c630050979
Joerg <joerg@berlin-byt.es>
2021-10-05 13:52:16 +0200

    tests added

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,11 +1,16 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
+import request from "supertest";
 import { app } from "../app";
+
+declare global {
+  var signup: () => Promise<string[]>;
+}
 
 let mongo: any;
 beforeAll(async () => {
   process.env.JWT_KEY = "testtesttest";
-  
+
   mongo = await MongoMemoryServer.create();
   const mongoUri = mongo.getUri();
 
@@ -24,3 +29,17 @@ afterAll(async () => {
   await mongo.stop();
   await mongoose.connection.close();
 });
+
+global.signup = async () => {
+  const email = "test@test.com";
+  const password = "password";
+
+  const response = await request(app)
+    .post("/api/users/signup")
+    .send({ email, password })
+    .expect(201);
+
+  const cookie = response.get("Set-Cookie");
+
+  return cookie;
+};
