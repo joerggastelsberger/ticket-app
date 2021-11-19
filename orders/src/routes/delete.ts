5b14b35fa678ad58763601fd288a4c527620dbb1
Joerg <joerg@berlin-byt.es>
2021-11-19 10:59:31 +0100

    events in route handlers published

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -5,6 +5,8 @@ import {
   NotAuthorizedError,
 } from "@joerggasttickets/common";
 import { Order, OrderStatus } from "../models/order";
+import { OrderCancelledPublisher } from "../events/publishers/order-cancelled-publisher";
+import { natsWrapper } from "../nats-wrapper";
 
 const router = express.Router();
 
@@ -23,6 +25,13 @@ router.delete(
 
     order.status = OrderStatus.Cancelled;
     await order.save();
+
+    new OrderCancelledPublisher(natsWrapper.client).publish({
+      id: order.id,
+      ticket: {
+        id: order.ticket.id
+      },
+    });
 
     res.status(204).send(order);
   }
