dbf83433d04da1c368a6a69809807dab63617441
Joerg <joerg@berlin-byt.es>
2021-12-14 15:05:44 +0100

    stripe modal added

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
+import StripeCheckout from "react-stripe-checkout";
 
-const OrderShow = ({ order }) => {
-  const [timeLeft, setTimeLeft] = useState("");
+const OrderShow = ({ order, currentUser }) => {
+  const [timeLeft, setTimeLeft] = useState(0);
 
   useEffect(() => {
     const findTimeLeft = () => {
@@ -17,7 +18,21 @@ const OrderShow = ({ order }) => {
     };
   }, []);
 
-  return <div>Time left to pay: {timeLeft} seconds</div>;
+  if (timeLeft < 0) {
+    return <div>Order expired</div>;
+  }
+
+  return (
+    <div>
+      Time left to pay: {timeLeft} seconds
+      <StripeCheckout
+        token={(token) => console.log(token)}
+        stripeKey="pk_test_51K4l46E7zd5xLHzhW0hyG7UKOcjWceoLN7ZNrSVxAyOWpnb1g3S8fW9FOW8wf7aeCTqsOOlfKC5doz3WAFqHubws00jEHFCidw"
+        amount={order.ticket.price * 100}
+        email={currentUser.email}
+      />
+    </div>
+  );
 };
 
 OrderShow.getInitialProps = async (context, client) => {
